e0f7d1b624a70565140f15d823bf3c919f73d3ee
Evgenia Gurova <evgenia@montel.no>
2021-11-09 18:00:02 +0100

    api key

--- src/App.js
+++ src/App.js
@@ -9,19 +9,26 @@ import {
 } from "react-router-dom";
 import React from 'react';
 import CatPicturePage from './pages/cat-picture/CatPicturePage';
+import axios from 'axios';
 
 function App() {
+
+    axios.defaults.headers.common = {
+        "X-API-Key": "b44f38ab-ca02-40f5-b146-3cccefe2e942",
+    };
+
     return (
         <Router>
             <div className="App">
                 <main>
                     <div>
-                        <Link to="/">Todos</Link>
+                        <Link to="/todos">Todos</Link>
                         <Link to="/cats">Cats</Link>
                         <Link to="/cat-picture">CatPicture</Link>
                     </div>
                     <Routes>
-                        <Route path="/" element={<TodoListPage />} />
+                        <Route path="/" element={<CatPicturePage />} />
+                        <Route path="/todos" element={<TodoListPage />} />
                         <Route path="/cats" element={<CatsPage />} />
                         <Route path="/cat-picture" element={<CatPicturePage />} />
                     </Routes>
